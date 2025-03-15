2ff3948e9f12bee9d52bc92bc70f32490969586d
sangam2ishra <sangamkr.mishra@gmail.com>
2025-03-15 05:30:06 +0530

    product model creation

--- myproject/product/models.py
+++ myproject/product/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from mongoengine import *
 
-# Create your models here.
+
+class Product(Document):
+    name = StringField(max_length=200)
+    description = StringField()
+    category = StringField()
+    price = FloatField(required=True)
+    brand = StringField()
+    quantity = IntField()
+
+    def __str__(self):
+        return self.name
